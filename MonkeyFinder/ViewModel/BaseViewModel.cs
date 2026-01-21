241851b4e48b5343422c5d7bbd51907c39e6954d
Supradeep Mukherjee <supradeep2004@gmail.com>
2026-01-21 19:39:40 +0530

    Navigation(Data display pending)

---
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MonkeyFinder.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    public partial bool IsBusy { get; set; }
+
+    [ObservableProperty]
+    public partial string Title { get; set; }
+
+    public bool IsNotBusy => !IsBusy;
+}
+
---
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,57 @@
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class MonkeysViewModel : BaseViewModel
+{
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+    MonkeyService monkeyService;
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
+
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to get monkeys: {ex.Message}");
+            await Shell.Current.DisplayAlertAsync("Error!", ex.Message, "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+
+    }
+
+    [RelayCommand]
+    async Task ViewMonkeyAsync(Monkey monkey)
+    {
+        if (monkey is not null)
+        {
+            await Shell.Current.GoToAsync(
+                nameof(DetailsPage),
+                true,
+                new Dictionary<string, object> { { "Monkey", monkey } }
+            );
+        }
+    }
+}
